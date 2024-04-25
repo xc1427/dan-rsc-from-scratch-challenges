0415b44694f01776ba999ba01e7e18a0b3a253d6
Chen XI <xichen1427@gmail.com>
2024-04-25 08:58:28 +0000

    feat: 2-support-for-fragment

--- packages/2-support-for-fragment/client.js
+++ packages/2-support-for-fragment/client.js
@@ -26,7 +26,15 @@ async function fetchClientJSX(pathname) {
 function parseJSX(key, value) {
   if (value === "$RE") {
     return Symbol.for("react.element");
-  } else if (typeof value === "string" && value.startsWith("$$")) {
+  } 
+
+  /** ====== */
+  else if (value === "$RF") {
+    return Symbol.for("react.fragment");
+  }
+  /** ====== */
+  
+  else if (typeof value === "string" && value.startsWith("$$")) {
     return value.slice(1);
   } else {
     return value;
